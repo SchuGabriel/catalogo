2901d931455c23213f2bb800463bfb4876eaa6ac
SchuGabriel <schu-gabriel@hotmail.com>
2024-07-12 20:25:07 -0300

    seeder

---
+++ seeders/20240712230702-demo-user.js
@@ -0,0 +1,26 @@
+// Exemplo de seeder para inserir um usuário inicial na tabela Users
+
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Hash da senha do usuário (coloque a senha desejada)
+    const hashedPassword = await bcrypt.hash('password', 10);
+
+    // Inserir um usuário inicial na tabela Users
+    return queryInterface.bulkInsert('Users', [{
+      name: 'Schuzao',
+      email: 'schu-gabriel@hotmail.com',
+      password: hashedPassword,
+      role: 'user',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Remover o usuário inicial inserido
+    return queryInterface.bulkDelete('Users', { email: 'test@example.com' }, {});
+  }
+};
